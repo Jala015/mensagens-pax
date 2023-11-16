ad30654c67eb94116ebf4ee046bf5b7400a42fce
Felipe Simões <felipebouabci@gmail.com>
2023-11-16 13:15:36 -0300

    testes 3

--- src/components/obter-contatos.svelte
+++ src/components/obter-contatos.svelte
@@ -1,6 +1,7 @@
 <script>
   async function getContacts() {
     try {
+        const props = await navigator.contacts.getProperties();
       const contacts = await navigator.contacts.select(props, {multiple:true});
       alert(JSON.stringify(contacts));
     } catch (ex) {
