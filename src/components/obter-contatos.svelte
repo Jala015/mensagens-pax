17423b454c832a5657dd90efcd68f82ac7d331db
Felipe Simões <felipebouabci@gmail.com>
2023-11-16 13:23:14 -0300

    lista de contatos

--- src/components/obter-contatos.svelte
+++ src/components/obter-contatos.svelte
@@ -1,14 +1,32 @@
 <script>
+  import { contatos } from "@scripts/store";
+
+    let contatosLocal = []
+    let pronto = false
+
+    function formatarResultados(r){
+        r.forEach((contato)=>{
+            contatosLocal = [...contatosLocal, {nome: contato.name ?? '', contato: contato.tel[0] ?? undefined}]
+        })
+        contatos.set(contatosLocal)
+        pronto = true
+    }
+
   async function getContacts() {
     try {
         const props = await navigator.contacts.getProperties();
       const contacts = await navigator.contacts.select(props, {multiple:true});
-      alert(JSON.stringify(contacts));
+      formatarResultados(contacts)
     } catch (ex) {
       alert(ex)
     }
   }
+
+
 </script>
 
 <button on:click={getContacts}>Importar contatos</button>
 
+{#if pronto}
+<a href="/lista-de-contatos" role="button">Próximo</a>
+{/if}
